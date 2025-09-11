ce46c465ae40633b5d8a67e7e4f9aef0ece3f20d
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-09-11 09:22:07 +0700

    Percobaan 2

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -1,8 +1,30 @@
 public class Motor {
-    public String platNomor;
-    public boolean statusMesin;
-    public int kecepatan;
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
 
+    public String getPlatNomor(){
+        return platNomor;
+    }
+    public void setPlatNomor(String platNomor){
+        this.platNomor = platNomor;
+    }
+    public boolean getStatusMesin(){
+        return statusMesin;
+    }
+    public void setStatusMesin(boolean statusMesin){
+        this.statusMesin = statusMesin;
+    }
+    public int getKecepatan(){
+        return kecepatan;
+    }
+    public void setKecepatan(int kecepatan){
+        if(!this.statusMesin && kecepatan > 0){
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        }else{
+            this.kecepatan = kecepatan;
+        }
+    }
     public void displayInfo(){
         System.out.println("Plat Nomor: " + this.platNomor);
         System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));
